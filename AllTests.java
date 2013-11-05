8a2f100be9545ed63e7673b5547ed214a198e5bc
suryabhatt <suryabhatt.sun@gmail.com>
2013-11-05 00:56:25 -0500

    Create AllTests.java
    Test File

---
+++ AllTests.java
@@ -0,0 +1,14 @@
+//Submitted by: Surya Bhatt
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+//For testing all the classes
+@RunWith(Suite.class)
+@SuiteClasses({TAScheduleTest.class})
+public class AllTests {
+
+	
+	
+}
