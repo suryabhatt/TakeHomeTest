87fb3c2eedfab4b30f2751b8257bedc59bac526a
suryabhatt <suryabhatt.sun@gmail.com>
2013-11-05 00:59:23 -0500

    Create Time.java

---
+++ Time.java
@@ -0,0 +1,33 @@
+//Submitted By: Surya Bhatt
+
+
+public class Time {
+
+	private String day;
+	private int timeOfDay;
+	
+	static final String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"};
+	
+	public Time() {
+		// TODO Auto-generated constructor stub
+	}
+	//return the day of the class
+	public String getDay(MeetingTime mt){
+		
+		int d;
+		d= mt.getStartTime()/(24*60);
+		
+		return days[d];
+		
+		
+	}
+	//return the time for a particular class or lab
+	public int getTimeOfDay(MeetingTime mt){
+		
+		int minutesOfDay = mt.getStartTime()%(60*24);
+		
+		
+		return minutesOfDay;
+	}
+
+}
